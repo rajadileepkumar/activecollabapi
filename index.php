181ab8457811b81e2604d492e42c107e41211261
rajadileepkumar <rajadileepkumar34@gmail.com>
2018-04-16 19:00:16 +0530

    subtasks_tasks rendering started

--- index.php
+++ index.php
@@ -161,103 +161,131 @@ class ActiveCollabAPI{
 		$authenticator = new \ActiveCollab\SDK\Authenticator\Cloud($active_Unserilizedarry['2'], 'My Awesome Application',$active_Unserilizedarry['3'],$active_Unserilizedarry['4']);
 
 		$token = $authenticator->issueToken((int) $active_Unserilizedarry['1']);
-		
+		$client;
 		if($token){
 			$client = new \ActiveCollab\SDK\Client($token);
 			$user = $authenticator->getUser();
 			$projectName = $client->get('projects/'.$active_Unserilizedarry['5'])->getJson();
-		}
-
-		?>
-		<div class="wrap">
 			
-			<h2>Active Collab<sub>/ <?php echo $user['first_name'].$user['last_name'];?></sub><sub>/ <?php echo $projectName['single']['name']; ?></sub></h2>
-
-			<?php
-				$taskList = $client->get('projects/'.$active_Unserilizedarry['5'].'/tasks')->getJson();
-				//print_r($taskList);
-				$taskarray = array();
-				foreach ($taskList['tasks'] as $tsklist) {
-					$taskarray[$tsklist['name']]['taskid'] = $tsklist['task_list_id'];
-					$taskarray[$tsklist['name']]['label'] = $tsklist['labels'][0]['name'];
-					$taskarray[$tsklist['name']]['color'] = $tsklist['labels'][0]['color'];
-					$taskarray[$tsklist['name']]['assignee_name'] = $tsklist['fake_assignee_name'];
-					$taskarray[$tsklist['name']]['open_sub_task'] = $tsklist['open_subtasks'];
-					$taskarray[$tsklist['name']]['task_url_path'] = $tsklist['url_path'];
-				}
-				//var_dump($taskarray);
-
-				foreach ($taskList['task_lists'] as $list) {
-					?>
-						<div class="postbox-container">
-							<div class="postbox">
-								<h2 class="hndle ui-sortable-handle	">
-									<?php
-										$task_list_id = $list['id'];
-									?>
-									<span><?php echo $list['name']?></span>
-								</h2>
-								<div class="inside">
-									<div class="main">
-										<?php  
-											foreach ($taskarray as $key => $value) {
-												if($value['taskid'] == $task_list_id){
-													$taskclass = str_replace(' ', '-', strtolower($key));
-													if(!empty($value['assignee_name'])){
-														$assignee = $value['assignee_name'];	
-													}
-													echo '<div data-toggle="modal" class="task" id="tasklist-'.$value['taskid'].'-'.$taskclass.'" href="#tasklist-'.$value['taskid'].'-'.$taskclass.'">';
-														
-														echo '<span class="task-name">'.$key.'<sub>-'.$assignee.'</span>';
-														
-														if(!empty($value['label'])){
-															echo '<span class="task-label" style="color:'.$value['color'].'">'.$value['label'].'</span>';
-														}
-
-														if(!empty($value['open_sub_task'])){
-															echo '<span class="task-icons dashicons dashicons-editor-ul">'.$value['open_sub_task'].'</span>';
-														}
-														
-													echo '</div>';
-
-													?>	
-													<?php			
-												}
-												?>
-												<div id="tasklist-<?php echo $value['taskid'].'-'.$taskclass ?>" class="modal fade" role="dialog">
-															<div class="modal-dialog">
-															    <!-- Modal content-->
-															    <div class="modal-content">
-																      <div class="modal-header">
-																      		<button type="button" class="close" data-dismiss="modal">&times;</button>
-																        	<h4 class="modal-title">Modal Header</h4>
-																      </div>
-																      <div class="modal-body">
-																        	<p><?php echo $value['task_url_path']; ?></p>
-																      </div>
-																      <div class="modal-footer">
-																      		<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-																      </div>
-															    </div>
-															</div>
-														</div>
-											<?php
-												
-											}
-
-										?>
-									</div>
-								</div>
-							</div>
-						</div> 
+		}
+		?>
+			<div class="wrap">
+				<h2>Active Collab<sub>/ <?php echo $user['first_name'].$user['last_name'];?></sub><sub>/ <?php echo $projectName['single']['name']; ?></sub>
+				</h2>
+				<div class="postbox-container">
 					<?php
-				}
-			?>
-		</div>
+						$taskList = $client->get('projects/'.$active_Unserilizedarry['5'].'/tasks')->getJson();
+						//print_r($taskList);
+						$taskarray = array();
+						foreach ($taskList['tasks'] as $tsklist) {
+							$taskarray[$tsklist['name']]['taskid'] = $tsklist['task_list_id'];
+							$taskarray[$tsklist['name']]['label'] = $tsklist['labels'][0]['name'];
+							$taskarray[$tsklist['name']]['color'] = $tsklist['labels'][0]['color'];
+							$taskarray[$tsklist['name']]['assignee_name'] = $tsklist['fake_assignee_name'];
+							$taskarray[$tsklist['name']]['open_sub_task'] = $tsklist['open_subtasks'];
+							$taskarray[$tsklist['name']]['task_url_path'] = $tsklist['url_path'];
+						}
+						//var_dump($taskarray);
+						foreach ($taskList['task_lists'] as $list) {
+							?>
+								<div class="postbox">
+									<h2 class="hndle ui-sortable-handle	">
+										<?php
+											$task_list_id = $list['id'];
+											$task_list_total = $list['open_tasks']; //count of tasks
+										?>
+										<span><?php echo $list['name'];?> (<?php echo $task_list_total; ?>)</span>
+									</h2>
+									<div class="inside">
+										<div class="main">
+											<?php  
+												foreach ($taskarray as $key => $value) {
+													if($value['taskid'] == $task_list_id){
+														$taskclass = str_replace(' ', '-', strtolower($key));
+														if(!empty($value['assignee_name'])){
+															$assignee = $value['assignee_name'];	
+														}
+														echo '<div data-toggle="modal" class="task" href="'.$value['task_url_path'].'" data-target="#tasklist-'.$value['taskid'].'-'.$taskclass.'">';
+															
+															echo '<span class="task-name">'.$key.'<sub>-'.$assignee.'</span>';
+															
+															if(!empty($value['label'])){
+																echo '<span class="task-label" style="color:'.$value['color'].'">'.$value['label'].'</span>';
+															}
+
+															if(!empty($value['open_sub_task'])){
+																echo '<span class="task-icons dashicons dashicons-editor-ul">'.$value['open_sub_task'].'</span>';
+															}
+															
+														echo '</div>';
+
+														?>	
+														<?php			
+													}
+													?>
+														<div id="tasklist-<?php echo $value['taskid'].'-'.$taskclass ?>" class="modal fade" role="dialog">
+															<div class="modal-dialog">
+														    	<div class="modal-content">
+															    	<div class="modal-header">
+															        	<button type="button" class="close" data-dismiss="modal">&times;</button>
+															        	<h4 class="modal-title">Modal Header</h4>
+															      	</div>
+															    	<div class="modal-body">
+															        	<h2><?php echo $key; ?></h2>
+															        	<?php
+															        		$subtask_List = $client->get($value['task_url_path'])->getJson();
+															        		//print_r($subtask_List['subtasks']);
+															        		$subtaskArray = array();
+															        		$commentsArray = array();
+															        		foreach ($subtask_List['subtasks'] as $slist) {
+															        			//$subtaskArray[$slist['name']]['name'] = $slist['name'];
+															        			//print_r($slist);
+															        			//echo $slist['is_completed'];
+															        			if($slist['is_completed'] == ''){
+															        					echo '<p>open task'.$slist['name'].'</p>';	
+															        				}
+															        		}
+
+
+															        	?>
+															      	</div>
+															    	<div class="modal-footer">
+															        	<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+															      	</div>
+														   		 </div><!--modal-content-->
+														  	</div><!--modal-dialog-->
+														</div><!--modal-->
+													<?php
+												}
+											?>
+										</div><!--main-->	
+									</div><!--inside-->
+								</div><!--postbox-->
+							<?php
+						}
+					?>
+				</div><!--postbox-container-->
+			</div><!--wrap-->
 		<?php
 	}
 
-	
+	private function subtasks_tasks($subtask_List){
+		global $wpdb;
+		$userId = get_current_user_id();
+		$optionName = 'active_collab_setting_'.$userId; 
+		$settings =get_option($optionName, false);
+		$active_Unserilizedarry = unserialize($settings);
+		
+		$authenticator = new \ActiveCollab\SDK\Authenticator\Cloud($active_Unserilizedarry['2'], 'My Awesome Application',$active_Unserilizedarry['3'],$active_Unserilizedarry['4']);
+
+		$token = $authenticator->issueToken((int) $active_Unserilizedarry['1']);
+		
+		if($token){
+			$client = new \ActiveCollab\SDK\Client($token);
+			$subtask_List = $client->get($subtask_List.'/subtasks')->getJson();
+			print_r($subtask_List);
+		}
+	}
 }
 
 $obj = new ActiveCollabAPI();
